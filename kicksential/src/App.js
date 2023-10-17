870a2452a3de1396a72feb9f48153ab3af30fbf4
makachikava <chikavamakanaka@gmail.com>
2023-10-16 20:05:29 -0700

    added fake cards, search bar, category buttons

--- kicksential/src/App.js
+++ kicksential/src/App.js
@@ -3,13 +3,64 @@ import './App.css';
 
 function App() {
   return (
-    <div className='flex'>
+  <div className='flex'>
       <Filter/>
       <div className='flex flex-col w-full h-screen bg-red-500'>
         <h1>Main container</h1>
-      </div>
+        <input 
+        type='search'
+        className=''
+        placeholder='Search item'/>
 
-    </div>
+        <div className='flex justify-around'>
+          <button className='bg-gray-50 rounded mt-5'>category</button>
+          <button className='bg-gray-50 rounded mt-5'>category</button>
+          <button className='bg-gray-50 rounded mt-5'>category</button>
+          <button className='bg-gray-50 rounded mt-5'>category</button>
+          <button className='bg-gray-50 rounded mt-5'>category</button>
+        </div>
+
+        <div className='flex flex-row flex-wrap justify-center items-center justify-around bg-gray-500 w-full h-full'>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card1</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card2</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card3</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card4</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card5</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card6</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card7</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card7</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card7</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card7</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card7</h1>
+          </div>
+          <div className='bg-yellow-500 h-60 w-60 rounded'>
+            <h1>card7</h1>
+          </div>
+
+        </div>
+      </div>
+  </div>
   );
 }
 
