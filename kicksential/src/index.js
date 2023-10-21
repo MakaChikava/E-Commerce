5a854a78b3f4401a061fb90dc7a849569d60aae9
makachikava <chikavamakanaka@gmail.com>
2023-10-20 20:09:16 -0700

    created pages with react-router and created seperate component for navbar

--- kicksential/src/index.js
+++ kicksential/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import Cart from './cart';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/cart",
+    element: <Cart/>,
+  }
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
