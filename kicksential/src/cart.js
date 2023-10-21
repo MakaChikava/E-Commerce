f10d11e8d8ed835f29f9ee70c0c8b467beb37c27
makachikava <chikavamakanaka@gmail.com>
2023-10-20 22:44:10 -0700

    working on cart

--- kicksential/src/cart.js
+++ kicksential/src/cart.js
@@ -1,8 +1,15 @@
-
+import Navbar from "./components/Navbar";
 const Cart = () => {
     return (
-        <div>
-            <h1>Cart</h1>
+        <div className="flex flex-col h-screen">
+            <Navbar/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div className="flex justify-center">
+                <h1 className="text-red-500 text-2xl">Your Cart Items (0)</h1>
+            </div>
         </div>
     );
 }
