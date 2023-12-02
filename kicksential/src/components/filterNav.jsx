e6fd0821e27f18170e8e4b0f3aa3cf4b390b782b
makachikava <chikavamakanaka@gmail.com>
2023-12-01 22:02:13 -0800

    upsdate

--- kicksential/src/components/filterNav.jsx
+++ kicksential/src/components/filterNav.jsx
@@ -26,18 +26,17 @@ const filter = (props) => {
                 <h1 className="flex ml-2 font-bold text-lg text-gray-700">COLOR</h1>
 
                 <div className="flex flex-row flex-wrap w-5/6">
-                <p className="m-5 w-5 h-5 rounded-full bg-green-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-red-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-yellow-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-black"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-white"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-blue-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-orange-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-orange-950"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-gray-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-pink-500"></p>
-                <p className="m-5 w-5 h-5 rounded-full bg-purple-500"></p>
-
+                    <p className="m-5 w-5 h-5 rounded-full bg-green-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-red-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-yellow-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-black"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-white"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-blue-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-orange-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-orange-950"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-gray-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-pink-500"></p>
+                    <p className="m-5 w-5 h-5 rounded-full bg-purple-500"></p>
                 </div>
         
         </div>
