31f605e7189d5db17ed9ea20675c2757e6739cc7
makachikava <chikavamakanaka@gmail.com>
2023-12-07 16:14:34 -0800

    sign up post request works, there are some password requiurements that need to be fufilled

--- kicksential/src/signup.js
+++ kicksential/src/signup.js
@@ -51,7 +51,6 @@ const SignUp = () => {
                 username: username,
                 password: password
             }
-
             axios
                 .post('http://127.0.0.1:8000/api/v1/users/', signUpData)
                 .then((res)=>{
